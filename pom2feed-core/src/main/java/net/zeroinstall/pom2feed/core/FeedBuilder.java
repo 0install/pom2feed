2b511f5603e954c13cad257abf0fe820fc611207
Bastian Eicher <bastian@eicher.net>
2013-02-16 02:00:00 +0100

    Tweaked FeedBuilder

--- pom2feed-core/src/main/java/net/zeroinstall/pom2feed/core/FeedBuilder.java
+++ pom2feed-core/src/main/java/net/zeroinstall/pom2feed/core/FeedBuilder.java
@@ -2,13 +2,23 @@ package net.zeroinstall.pom2feed.core;
 
 import java.net.URI;
 import net.zeroinstall.model.*;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.model.*;
 import static org.apache.xmlbeans.XmlObject.Factory.newValue;
 
+/**
+ * Builds Zero Install feeds using data from Maven projects.
+ */
 public class FeedBuilder {
 
     private final URI pom2feedService;
 
+    /**
+     * Creates a new feed builder.
+     *
+     * @param pom2feedService The base URI of the pom2feed service used to
+     * provide dependencies.
+     */
     public FeedBuilder(URI pom2feedService) {
         this.pom2feedService = pom2feedService;
     }
@@ -17,10 +27,15 @@ public class FeedBuilder {
         Feed feed = Feed.Factory.newInstance();
         feed.addName(model.getName());
 
-        feed.addNewSummary().set(newValue(model.getDescription()));
-        feed.addHomepage(model.getUrl());
+        feed.addNewSummary().set(newValue(
+                StringUtils.isEmpty(model.getDescription())
+                ? "Maven artifact"
+                : model.getDescription()));
+        if (!StringUtils.isEmpty(model.getUrl())) {
+            feed.addHomepage(model.getUrl());
+        }
 
-        // TODO
+        // TODO: Convert more stuff
 
         return feed;
     }
