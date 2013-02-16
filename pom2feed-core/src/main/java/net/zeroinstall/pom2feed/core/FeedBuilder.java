a779e7076b59fa8c69b057e67b6f487c1ac97382
Bastian Eicher <bastian@eicher.net>
2013-02-16 11:22:05 +0100

    - Changed return type of generateFeed() to InterfaceDocument - Replaced XmlObject.Factory.newValue() with XmlObject.Factory.parse()

--- pom2feed-core/src/main/java/net/zeroinstall/pom2feed/core/FeedBuilder.java
+++ pom2feed-core/src/main/java/net/zeroinstall/pom2feed/core/FeedBuilder.java
@@ -1,10 +1,13 @@
 package net.zeroinstall.pom2feed.core;
 
 import com.google.common.base.Strings;
+import com.google.common.base.Throwables;
 import java.net.URI;
 import net.zeroinstall.model.*;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.maven.model.*;
-import static org.apache.xmlbeans.XmlObject.Factory.newValue;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlObject;
 
 /**
  * Builds Zero Install feeds using data from Maven projects.
@@ -20,51 +23,64 @@ public class FeedBuilder {
      * provide dependencies.
      */
     public FeedBuilder(URI pom2feedService) {
-	this.pom2feedService = pom2feedService;
+        this.pom2feedService = pom2feedService;
     }
 
-    public Feed generateFeed(Model model) {
-	Feed feed = Feed.Factory.newInstance();
-	feed.addName(model.getName());
+    public InterfaceDocument generateFeed(Model model) throws XmlException {
+        InterfaceDocument document = InterfaceDocument.Factory.newInstance();
+        Feed feed = document.addNewInterface();
 
-	feed.addNewSummary().set(newValue(
-		Strings.isNullOrEmpty(model.getDescription())
-		? "Maven artifact"
-		: model.getDescription()));
-	if (!Strings.isNullOrEmpty(model.getUrl())) {
-	    feed.addHomepage(model.getUrl());
-	}
+        feed.addName(model.getName());
+        feed.addNewSummary().set(plainText(
+                Strings.isNullOrEmpty(model.getDescription())
+                ? "Maven artifact"
+                : model.getDescription()));
+        if (!Strings.isNullOrEmpty(model.getUrl())) {
+            feed.addHomepage(model.getUrl());
+        }
 
-	// TODO: Convert more stuff
+        // TODO: Convert more stuff
 
-	return feed;
+        return document;
     }
 
     public Implementation addLocalImplementation(Feed feed, Model model) {
-	Implementation impl = addImplementation(feed, model);
-	impl.setLocalPath(".");
-	return impl;
+        Implementation impl = addImplementation(feed, model);
+        impl.setLocalPath(".");
+        return impl;
     }
 
     public Implementation addRemoteImplementation(Feed feed, Model model, URI jarUri) {
-	Implementation impl = addImplementation(feed, model);
-	// TODO: Add <file>
-	return impl;
+        Implementation impl = addImplementation(feed, model);
+        // TODO: Add <file>
+        return impl;
     }
 
     private Implementation addImplementation(Feed feed, Model model) {
-	Implementation impl = feed.addNewImplementation();
+        Implementation impl = feed.addNewImplementation();
+        impl.setVersion(pom2feedVersion(model.getVersion()));
 
-	// TODO: Convert Maven version numbers to Zero Install version numbers
-	impl.setVersion(model.getVersion());
+        for (org.apache.maven.model.Dependency mavenDep : model.getDependencies()) {
+            net.zeroinstall.model.Dependency ziDep = impl.addNewRequires();
+            ziDep.setInterface(pom2feedService.toString()
+                    // TODO: Transform to proper feed URI
+                    + mavenDep.getGroupId() + "/" + mavenDep.getArtifactId());
+            ziDep.setVersion(pom2feedVersion(mavenDep.getVersion()));
+        }
 
-	for (org.apache.maven.model.Dependency dependency : model.getDependencies()) {
-	    net.zeroinstall.model.Dependency dep = impl.addNewRequires();
-	    dep.setInterface(pom2feedService.toString()
-		    // TODO: Transform to proper feed URI
-		    + dependency.getGroupId() + "/" + dependency.getArtifactId());
-	}
+        return impl;
+    }
 
-	return impl;
+    private static XmlObject plainText(String value) {
+        try {
+            return XmlObject.Factory.parse(StringEscapeUtils.escapeXml(value));
+        } catch (XmlException ex) {
+            throw Throwables.propagate(ex);
+        }
+    }
+
+    private static String pom2feedVersion(String pomVersion) {
+        // TODO: Handle -snapshot, rc, etc.
+        return pomVersion;
     }
 }
