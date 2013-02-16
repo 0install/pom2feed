c2c562f48b69956963ca745f82315dcf2e46e291
Simon E. Silva Lauinger <simon@silvalauinger.de>
2013-02-16 02:15:38 +0100

    Remove "commons-lang3" as dependency, because guava has string util methods too.

--- pom2feed-core/src/main/java/net/zeroinstall/pom2feed/core/FeedBuilder.java
+++ pom2feed-core/src/main/java/net/zeroinstall/pom2feed/core/FeedBuilder.java
@@ -1,8 +1,8 @@
 package net.zeroinstall.pom2feed.core;
 
+import com.google.common.base.Strings;
 import java.net.URI;
 import net.zeroinstall.model.*;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.model.*;
 import static org.apache.xmlbeans.XmlObject.Factory.newValue;
 
@@ -20,51 +20,51 @@ public class FeedBuilder {
      * provide dependencies.
      */
     public FeedBuilder(URI pom2feedService) {
-        this.pom2feedService = pom2feedService;
+	this.pom2feedService = pom2feedService;
     }
 
     public Feed generateFeed(Model model) {
-        Feed feed = Feed.Factory.newInstance();
-        feed.addName(model.getName());
+	Feed feed = Feed.Factory.newInstance();
+	feed.addName(model.getName());
 
-        feed.addNewSummary().set(newValue(
-                StringUtils.isEmpty(model.getDescription())
-                ? "Maven artifact"
-                : model.getDescription()));
-        if (!StringUtils.isEmpty(model.getUrl())) {
-            feed.addHomepage(model.getUrl());
-        }
+	feed.addNewSummary().set(newValue(
+		Strings.isNullOrEmpty(model.getDescription())
+		? "Maven artifact"
+		: model.getDescription()));
+	if (!Strings.isNullOrEmpty(model.getUrl())) {
+	    feed.addHomepage(model.getUrl());
+	}
 
-        // TODO: Convert more stuff
+	// TODO: Convert more stuff
 
-        return feed;
+	return feed;
     }
 
     public Implementation addLocalImplementation(Feed feed, Model model) {
-        Implementation impl = addImplementation(feed, model);
-        impl.setLocalPath(".");
-        return impl;
+	Implementation impl = addImplementation(feed, model);
+	impl.setLocalPath(".");
+	return impl;
     }
 
     public Implementation addRemoteImplementation(Feed feed, Model model, URI jarUri) {
-        Implementation impl = addImplementation(feed, model);
-        // TODO: Add <file>
-        return impl;
+	Implementation impl = addImplementation(feed, model);
+	// TODO: Add <file>
+	return impl;
     }
 
     private Implementation addImplementation(Feed feed, Model model) {
-        Implementation impl = feed.addNewImplementation();
+	Implementation impl = feed.addNewImplementation();
 
-        // TODO: Convert Maven version numbers to Zero Install version numbers
-        impl.setVersion(model.getVersion());
+	// TODO: Convert Maven version numbers to Zero Install version numbers
+	impl.setVersion(model.getVersion());
 
-        for (org.apache.maven.model.Dependency dependency : model.getDependencies()) {
-            net.zeroinstall.model.Dependency dep = impl.addNewRequires();
-            dep.setInterface(pom2feedService.toString()
-                    // TODO: Transform to proper feed URI
-                    + dependency.getGroupId() + "/" + dependency.getArtifactId());
-        }
+	for (org.apache.maven.model.Dependency dependency : model.getDependencies()) {
+	    net.zeroinstall.model.Dependency dep = impl.addNewRequires();
+	    dep.setInterface(pom2feedService.toString()
+		    // TODO: Transform to proper feed URI
+		    + dependency.getGroupId() + "/" + dependency.getArtifactId());
+	}
 
-        return impl;
+	return impl;
     }
 }
