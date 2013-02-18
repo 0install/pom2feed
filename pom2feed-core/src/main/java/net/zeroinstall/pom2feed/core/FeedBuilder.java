4a5b4e031b0fb2550d62592855696d2693a511a1
Bastian Eicher <bastian@eicher.net>
2013-02-18 15:09:21 +0100

    Improved FeedBuilder logic

--- pom2feed-core/src/main/java/net/zeroinstall/pom2feed/core/FeedBuilder.java
+++ pom2feed-core/src/main/java/net/zeroinstall/pom2feed/core/FeedBuilder.java
@@ -4,6 +4,7 @@ import com.google.common.base.Strings;
 import java.net.URI;
 import net.zeroinstall.model.*;
 import org.apache.maven.model.*;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
 
 /**
  * Iterativley builds Zero Install feeds using data from Maven projects.
@@ -59,9 +60,9 @@ public class FeedBuilder {
      * @return The implementation that was created and added to the feed.
      */
     public Implementation addLocalImplementation(Model model) {
-        Implementation impl = addImplementation(model);
-        impl.setLocalPath(".");
-        return impl;
+        Implementation implementation = addImplementation(model);
+        implementation.setLocalPath(".");
+        return implementation;
     }
 
     /**
@@ -73,17 +74,17 @@ public class FeedBuilder {
      * @return The implementation that was created and added to the feed.
      */
     public Implementation addRemoteImplementation(Model model, URI jarUri) {
-        Implementation impl = addImplementation(model);
+        Implementation implementation = addImplementation(model);
 
         String hash = "abc";
         long size = 123;
         String fileName = model.getBuild().getFinalName() + "." + model.getPackaging();
-        ManifestDigest digest = impl.addNewManifestDigest();
+        ManifestDigest digest = implementation.addNewManifestDigest();
         digest.setSha1New(FeedUtils.getSha1ManifestDigest(hash, size, fileName));
 
         // TODO: Add <file>
 
-        return impl;
+        return implementation;
     }
 
     /**
@@ -95,25 +96,51 @@ public class FeedBuilder {
      * @return The implementation that was created and added to the feed.
      */
     private Implementation addImplementation(Model model) {
-        Implementation impl = feed.addNewImplementation();
-        impl.setVersion(FeedUtils.pom2feedVersion(model.getVersion()));
-
+        Implementation implementation = feed.addNewImplementation();
+        implementation.setVersion(FeedUtils.pom2feedVersion(model.getVersion()));
         if (!model.getLicenses().isEmpty()) {
-            impl.setLicense(model.getLicenses().get(0).getName());
+            implementation.setLicense(model.getLicenses().get(0).getName());
         }
 
-        Command command = impl.addNewCommand();
+        addCommand(implementation, model);
+        addDependencies(implementation, model);
+
+        return implementation;
+    }
+
+    private void addCommand(Implementation implementation, Model model) {
+        Command command = implementation.addNewCommand();
         command.setName("run");
         command.setPath(model.getBuild().getFinalName() + "." + model.getPackaging());
 
+        Runner runner = command.addNewRunner();
+        runner.setInterface("http://repo.roscidus.com/java/openjdk-jre");
+        runner.addArg("-jar");
+    }
+
+    private void addDependencies(Implementation implementation, Model model) {
+        Plugin compilerPlugin = model.getBuild().getPluginsAsMap().get("org.apache.maven.plugins:maven-compiler-plugin");
+        if (compilerPlugin != null) {
+            Xpp3Dom config = (Xpp3Dom) compilerPlugin.getConfiguration();
+            String javaVersion = config.getChild("target").getValue();
+            if (!Strings.isNullOrEmpty(javaVersion)) {
+                net.zeroinstall.model.Dependency javaDep = implementation.addNewRequires();
+                javaDep.setInterface("http://repo.roscidus.com/java/openjdk-jre");
+                Constraint constraint = javaDep.addNewVersion2();
+                constraint.setNotBefore(javaVersion);
+            }
+        }
+
         for (org.apache.maven.model.Dependency mavenDep : model.getDependencies()) {
-            net.zeroinstall.model.Dependency ziDep = impl.addNewRequires();
+            net.zeroinstall.model.Dependency ziDep = implementation.addNewRequires();
             ziDep.setInterface(pom2feedService.toString()
                     // TODO: Transform to proper feed URI
                     + mavenDep.getGroupId() + "/" + mavenDep.getArtifactId());
             ziDep.setVersion(FeedUtils.pom2feedVersion(mavenDep.getVersion()));
-        }
 
-        return impl;
+            Environment environment = ziDep.addNewEnvironment();
+            environment.setName("CLASSPATH");
+            environment.setInsert(".");
+        }
     }
 }
