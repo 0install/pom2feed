6b0e7742a465b9c67175aaba113f601eda81e3d2
Simon E. Silva Lauinger <simon@silvalauinger.de>
2013-05-02 10:52:06 +0200

    Test is now a parameterized test.

--- pom2feed-service/src/test/java/net/zeroinstall/pom2feed/service/ArtifactUtilsTest.java
+++ pom2feed-service/src/test/java/net/zeroinstall/pom2feed/service/ArtifactUtilsTest.java
@@ -1,33 +1,43 @@
 package net.zeroinstall.pom2feed.service;
 
+import static com.google.common.collect.Lists.newArrayList;
+import java.util.Collection;
 import static net.zeroinstall.pom2feed.service.ArtifactUtils.*;
 import org.junit.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class ArtifactUtilsTest {
 
-    @Test
-    public void testValidatePathNoSlashAtEnd() {
-        assertFalse(validatePath("group/artifact"));
+    private final String url;
+    private final boolean expectedResult;
+
+    public ArtifactUtilsTest(final String url, final boolean expectedResult) {
+        this.url = url;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return newArrayList(new Object[]{
+            "group/artifact", false
+        }, new Object[]{
+            "artifact", false
+        }, new Object[]{
+            "group.sub/artifact/", false
+        }, new Object[]{
+            "/group/artifact/", false
+        }, new Object[]{
+            "group/artifact/", true
+        });
     }
 
     @Test
-    public void testValidatePathTooShort() {
-        assertFalse(validatePath("artifact"));
-    }
-
-    @Test
-    public void testValidatePathInvalidCharacters() {
-        assertFalse(validatePath("group.sub/artifact/"));
-    }
-
-    @Test
-    public void testValidatePathSlashAtStart() {
-        assertFalse(validatePath("/group/artifact/"));
-    }
-
-    @Test
-    public void testValidatePathValid() {
-        assertTrue(validatePath("group/artifact/"));
+    public void testValidatePath() {
+        assertThat(validatePath(url), is(expectedResult));
     }
 }
