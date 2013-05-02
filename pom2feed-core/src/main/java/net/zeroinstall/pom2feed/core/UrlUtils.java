8085d8cede61b789ef1cdcc7592d8287470b37d4
Simon E. Silva Lauinger <simon@silvalauinger.de>
2013-05-02 11:32:18 +0200

    Refractor: removed netbeans comment at beginning.

--- pom2feed-core/src/main/java/net/zeroinstall/pom2feed/core/UrlUtils.java
+++ pom2feed-core/src/main/java/net/zeroinstall/pom2feed/core/UrlUtils.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.zeroinstall.pom2feed.core;
 
 import static com.google.common.base.Preconditions.checkNotNull;
